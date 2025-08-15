4ded016548117e7d0bbfcb601ff0a6e6d88e5207
Getaye Temesgen <tgech71@gmail.com>
2025-08-15 13:57:32 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -6,14 +6,11 @@ const prismaClientSingleton = () => {
 }
 
 declare global {
-  // eslint-disable-next-line no-var
-  var prisma: ReturnType<typeof prismaClientSingleton> | undefined
+  var prisma: undefined | ReturnType<typeof prismaClientSingleton>
 }
 
 const prisma = globalThis.prisma ?? prismaClientSingleton()
 
 export { prisma }
 
-if (process.env.NODE_ENV !== 'production') {
-  globalThis.prisma = prisma
-}
+if (process.env.NODE_ENV !== 'production') globalThis.prisma = prisma
