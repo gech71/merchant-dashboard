c5f49ae83b6e4329980e903f56b9b8a995eb10a3
Getaye Temesgen <tgech71@gmail.com>
2025-08-18 09:03:01 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -6,11 +6,11 @@ const prismaClientSingleton = () => {
 }
 
 declare global {
-  var prismaGlobal: undefined | ReturnType<typeof prismaClientSingleton>
+  var prisma: undefined | ReturnType<typeof prismaClientSingleton>
 }
 
-const prisma = globalThis.prismaGlobal ?? prismaClientSingleton()
+const prisma = globalThis.prisma ?? prismaClientSingleton()
 
 export { prisma }
 
-if (process.env.NODE_ENV !== 'production') globalThis.prismaGlobal = prisma
+if (process.env.NODE_ENV !== 'production') globalThis.prisma = prisma
